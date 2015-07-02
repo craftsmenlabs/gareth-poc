9c2550bac5e2bee7668712a2d7f94baac4f23303
Hylke Stapersma <hylke.stapersma@gmail.com>
2015-07-02 17:14:36 +0200

    Added sample log + demo web application

--- demo-site/server.js
+++ demo-site/server.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var http = require('http');
- 
+var express = require('express')
+    , http = require('http')
+    , winston = require('winston');
+
 var expressApp = express();
+
+var i = 0;
+
 expressApp.use(express.static(__dirname + '/public'));
- 
+expressApp.get("/login", function (req, response) {
+    var statusCodes = [200, 403];
+    var statusCode = statusCodes[i];
+    if (i >= statusCodes.length-1) {
+        i = 0;
+    } else {
+        i++
+    }
+    winston.info(statusCode);
+    response.status(statusCode).end();
+
+});
+
 var listeningPort = 8888;
-http.createServer(expressApp).listen(listeningPort)
- 
+http.createServer(expressApp).listen(listeningPort);
+
 console.log('Server is listening on port ' + listeningPort);
